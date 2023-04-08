cc0d2a5ed539324f49533569eb081d4769c78d7c
Ashutosh Rana <103202211+ranaashutosh31@users.noreply.github.com>
2023-04-08 19:20:16 +0530

    Static initialization blocks @ HackerRank 8Apr23

---
+++ Basic Problems/staticInitializationBlocks.java
@@ -0,0 +1,39 @@
+/*
+You are given a class Solution with a main method. Complete the given code so that it outputs the area of a parallelogram with breadth  and height . You should read the variables from the standard input.
+
+If  or  , the output should be "java.lang.Exception: Breadth and height must be positive" without quotes.
+
+Input Format
+
+There are two lines of input. The first line contains : the breadth of the parallelogram. The next line contains : the height of the parallelogram.
+
+Constraints
+
+Output Format
+
+If both values are greater than zero, then the main method must output the area of the parallelogram. Otherwise, print "java.lang.Exception: Breadth and height must be positive" without quotes.
+
+Sample input 1
+
+1
+3
+Sample output 1
+
+3
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int B = in.nextInt();
+        int H = in.nextInt();
+        if(B>0 && H>0){
+            System.out.println(B*H);
+        }
+        else{
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+}
