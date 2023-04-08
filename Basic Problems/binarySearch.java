c6e8d06f69c9de46ba8f7243be80385ce729ccad
Ashutosh Rana <103202211+ranaashutosh31@users.noreply.github.com>
2023-04-08 19:34:26 +0530

    Binary Search @ GFG on 3Apr23

--- Basic Problems/binarySearch.java
+++ Basic Problems/binarySearch.java
@@ -1,4 +1,4 @@
-//Given sorted array of size N and an integer K, find the position(0-based indexing) at which K is present in the array using binary search.
+//Given a sorted array of size N and an integer K, find the position(0-based indexing) at which K is present in the array using binary search.
 /*Example 1:
 
 Input:
