5e4efe833ae6733fe2ee5cf461acebf65627a9b2
Ashutosh Rana <103202211+ranaashutosh31@users.noreply.github.com>
2023-04-20 01:02:14 +0530

    19Apr23 @ GFG - Remove Consecutive Characters
    https://practice.geeksforgeeks.org/problems/consecutive-elements2306/1?page=1&difficulty[]=-1&status[]=unsolved&category[]=Strings&curated[]=1&curated[]=8&curated[]=7&curated[]=2&curated[]=6&sortBy=submissions

--- Basic Problems/removeConsecutiveCharacters.java
+++ Basic Problems/removeConsecutiveCharacters.java
@@ -1,1 +1,28 @@
-//
+/*
+PROBLEM LINK IN EXTENDED DESCRIPTION
+
+Given a string S. For each index i(1<=i<=N-1), erase it if s[i] is equal to s[i-1] in the string.
+
+Example 1:
+Input:
+S = aabb
+Output:  ab 
+Explanation: 'a' at 2nd position is
+appearing 2nd time consecutively.
+Similiar explanation for b at
+4th position.
+*/
+
+class Solution{
+    public String removeConsecutiveCharacter(String S){
+        String ans = Character.toString(S.charAt(0));
+        int i=1;
+        while(i<S.length()){
+            if((S.charAt(i))!=(S.charAt(i-1))){
+                ans+=(S.charAt(i));
+            }
+            i++;
+        }
+        return ans;
+    }
+}
