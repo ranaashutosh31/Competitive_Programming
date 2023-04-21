2fb6bf9c30452723d3c3794758102760da24751d
Ashutosh Rana <103202211+ranaashutosh31@users.noreply.github.com>
2023-04-22 01:28:25 +0530

    21Apr23 @ GFG - Implement strstr
    https://practice.geeksforgeeks.org/problems/implement-strstr/1?page=1&difficulty[]=-1&status[]=unsolved&category[]=Strings&curated[]=1&curated[]=8&curated[]=7&curated[]=2&curated[]=6&sortBy=submissions

--- Basic Problems/implementStrstr.java
+++ Basic Problems/implementStrstr.java
@@ -1,1 +1,29 @@
-/**/
+/*
+PROBLEM LINK IN EXTENDED DESCRIPTION
+
+Your task is to implement the function strstr. The function takes two strings as arguments (s,x) and  locates the occurrence of the string x in the string s. The function returns and integer denoting the first occurrence of the string x in s (0 based indexing).
+Note: You are not allowed to use inbuilt function.
+
+Example 1:
+Input:
+s = GeeksForGeeks, x = Fr
+Output: -1
+Explanation: Fr is not present in the
+string GeeksForGeeks as substring.
+*/
+
+class GfG
+{
+    //Function to locate the occurrence of the string x in the string s.
+    int strstr(String s, String x)
+    {
+       // Your code here
+       int res=0;
+       if(s.contains(x)){
+           return s.indexOf(x);
+       }
+       else{
+           return -1;
+       }
+    }
+}
