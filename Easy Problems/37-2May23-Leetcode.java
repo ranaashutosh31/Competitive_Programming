c0cb0a55461555ef7d5aa0d035671cddef9c77f0
Ashutosh Rana <103202211+ranaashutosh31@users.noreply.github.com>
2023-05-02 23:47:15 +0530

    37 2May23 Sign of the Product of an Array
    https://leetcode.com/problems/sign-of-the-product-of-an-array/description/

---
+++ Easy Problems/37-2May23-Leetcode.java
@@ -0,0 +1,36 @@
+/*
+PROBLEM LINK IN EXTENDED DESCRIPTION
+There is a function signFunc(x) that returns:
+
+1 if x is positive.
+-1 if x is negative.
+0 if x is equal to 0.
+You are given an integer array nums. Let product be the product of all values in the array nums.
+
+Return signFunc(product).
+
+ 
+
+Example 1:
+Input: nums = [-1,-2,-3,-4,3,2,1]
+Output: 1
+Explanation: The product of all values in the array is 144, and signFunc(144) = 1
+*/
+
+class Solution {
+    public int arraySign(int[] nums) {
+        int product = 1;
+        return signFunc(nums,product);
+    }
+    public int signFunc(int[] nums,int product){
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                return 0;
+            }
+            else if(nums[i]<0){
+                product = -product;
+            }
+        }
+        return product;
+    }
+}
