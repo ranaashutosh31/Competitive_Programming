f97f6c215630d97ef837de0a6142e7bf1fddab74
Ashutosh Rana <103202211+ranaashutosh31@users.noreply.github.com>
2023-05-03 23:00:03 +0530

    38 3May23 Java Substring
    https://www.hackerrank.com/challenges/java-substring/problem?isFullScreen=false

--- Easy Problems/38-03May23-HackerRank.java
+++ Easy Problems/38-03May23-HackerRank.java
@@ -1,11 +1,10 @@
 /*
-PROBLEM LINK IN EXTENDED DESCRIPTION
+PROBLEM LINK IN EXTENDED DESCRIPTION (https://www.hackerrank.com/challenges/java-substring/problem?isFullScreen=false)
 Given a string, s, and two indices, start and end, print a substring consisting of all characters in the inclusive range from start to end. You'll find the String class' substring method helpful in completing this challenge.
 
 Input Format
 The first line contains a single string denoting s.
 The second line contains two space-separated integers denoting the respective values of start and end.
-
 Output Format
 Print the substring in the inclusive range from start to end-1.
 
