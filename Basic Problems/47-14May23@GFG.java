1219111bb867060e8cc3a73907754522e57ddf99
Ashutosh Rana <103202211+ranaashutosh31@users.noreply.github.com>
2023-05-14 22:02:26 +0530

    47 14May23 Count nodes of linked list
    https://practice.geeksforgeeks.org/problems/count-nodes-of-linked-list/1?page=1&difficulty[]=-2&difficulty[]=-1&status[]=unsolved&category[]=Linked%20List&sortBy=submissions

---
+++ Basic Problems/47-14May23@GFG.java
@@ -0,0 +1,28 @@
+/*
+PROBLEM LINK IN EXTENDED DESCRIPTION
+Given a singly linked list. The task is to find the length of the linked list, where length is defined as the number of nodes in the linked list.
+
+Example 1:
+Input:
+LinkedList: 1->2->3->4->5
+Output: 5
+Explanation: Count of nodes in the 
+linked list is 5, which is its length.
+*/
+
+class Solution
+{
+    //Function to count nodes of a linked list.
+    public static int getCount(Node head)
+    {
+        
+        //Code here
+        int count=0;
+        Node lastNode = head;
+        while(lastNode!=null){
+            lastNode = lastNode.next;
+            count++;
+        }
+        return count;
+    }
+}
