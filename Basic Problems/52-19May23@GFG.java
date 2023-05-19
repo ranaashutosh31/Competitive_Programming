5daa816890d8f3bd1f53cff6a94b73b53f0c55a0
Ashutosh Rana <103202211+ranaashutosh31@users.noreply.github.com>
2023-05-19 23:11:57 +0530

    52 19May23 Node at a given index in linked list
    https://practice.geeksforgeeks.org/problems/node-at-a-given-index-in-linked-list/1?page=1&difficulty[]=-1&status[]=unsolved&category[]=Linked%20List&sortBy=submissions

---
+++ Basic Problems/52-19May23@GFG.java
@@ -0,0 +1,35 @@
+/*
+PROBLEM LINK IN EXTENDED DESCRIPTION
+Given a singly linked list with N nodes and a number X. The task is to find the node at the given index (X)(1 based index) of linked list. 
+
+Input:
+First line of input contains number of testcases T. For each testcase, first line of input contains space seperated two integers, length of linked list and X.
+
+Output:
+For each testcase, there will be single line of output containing data at Xth node.
+
+Example:
+Input:
+2
+6 5
+1 2 3 4 657 76
+10 2
+8 7 10 8 6 1 20 91 21 2
+
+Output:
+657
+7
+
+*/
+
+class Solution {
+    
+    public void deleteAlternate (Node head){
+        //Write your code here
+        Node lastNode = head;
+        while(lastNode!=null){
+            lastNode = lastNode.next;
+            
+        }
+    }
+}
