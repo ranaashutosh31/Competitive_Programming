498c909c6433966ef917db4098960aa3f780eeb4
Ashutosh Rana <103202211+ranaashutosh31@users.noreply.github.com>
2023-05-27 13:57:05 +0530

    60 27May23 Factorial
    https://practice.geeksforgeeks.org/problems/factorial5739/1?page=1&difficulty[]=-1&status[]=unsolved&sortBy=submissions

---
+++ Basic Problems/60-27May23@GFG.java
@@ -0,0 +1,27 @@
+/*
+PROBLEM LINK IN EXTENDED DESCRIPTION
+Given a positive integer, N. Find the factorial of N.
+ 
+
+Example 1:
+Input:
+N = 5
+Output:
+120
+Explanation:
+5*4*3*2*1 = 120
+*/
+
+class Solution{
+    static long factorial(int N){
+        // code here
+        long fact = 1;
+        if(N==0 || N==1){
+            fact = fact*1;
+        }
+        for(int i=2;i<=N;i++){
+            fact = fact*i;
+        }
+        return fact;
+    }
+}
