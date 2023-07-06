21420b39eee79a0e0e4c300b659c42adecc56d05
Ashutosh Rana <103202211+ranaashutosh31@users.noreply.github.com>
2023-07-06 16:46:29 +0530

    94-06Jul23-Quick Sort (Med)
    https://practice.geeksforgeeks.org/problems/quick-sort/1

---
+++ POTD/94-06Jul23@GFG.java
@@ -0,0 +1,49 @@
+/*
+PROBLEM LINK IN EXTENDED DESCRIPTION
+Quick Sort is a Divide and Conquer algorithm. It picks an element as a pivot and partitions the given array around the picked pivot.
+Given an array arr[], its starting position is low (the index of the array) and its ending position is high(the index of the array).
+
+Note: The low and high are inclusive.
+
+Implement the partition() and quickSort() functions to sort the array.
+
+Example 1:
+Input: 
+N = 5 
+arr[] = { 4, 1, 3, 9, 7}
+Output:
+1 3 4 7 9
+*/
+
+class Solution
+{
+    //Function to sort an array using quick sort algorithm.
+    static void quickSort(int arr[], int low, int high)
+    {
+        // code here
+        if(low<high){
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex-1);
+            quickSort(arr, pivotIndex+1, high);
+        }
+        
+    }
+    static int partition(int arr[], int low, int high)
+    {
+        // your code here
+        int pivot=arr[high],i=low-1;
+        for(int j=low;j<high;j++){
+            if(arr[j]<pivot){
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = pivot;
+        arr[high] = temp;
+        return i;
+    } 
+}
